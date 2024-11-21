436787aaa910c43dca445441641e0a13fd36b457
Piyush Kumar Agarwal <chatwithapiyush@gmail.com>
2024-11-22 04:12:23 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 const userRoutes = require('./routes/user');
 const commentRoutes = require('./routes/comments');
@@ -20,8 +21,16 @@ app.use(express.json());
 app.use(cors());
 
 // routes
+app.use('/api', userRoutes);
 app.use('/api', commentRoutes);
 app.use('/api', saveRoutes);
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 // connect to mongodb
 mongoose.connect(process.env.MONGO_URI)
